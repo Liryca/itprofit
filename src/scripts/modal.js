09b109f89c0e5f209a8f1a14a815c54a13f0ad0a
Tarasova Anna <tarasavahanna@gmail.com>
2025-01-23 20:57:44 +0300

    fix

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,22 +1,22 @@
-window.addEventListener("load", () => {
-  const modal = document.getElementById("modal");
-  const openModalBtn = document.getElementById("openModal");
-  const closeModalBtn = document.getElementById("closeModal");
+// window.addEventListener("load", () => {
+const modal = document.getElementById("modal");
+const openModalBtn = document.getElementById("openModal");
+const closeModalBtn = document.getElementById("closeModal");
 
-  openModalBtn.addEventListener("click", () => {
-    modal.classList.add("active");
-    document.body.style.overflow = "hidden";
-  });
+openModalBtn.addEventListener("click", () => {
+  modal.classList.add("active");
+  document.body.style.overflow = "hidden";
+});
 
-  closeModalBtn?.addEventListener("click", () => {
+closeModalBtn?.addEventListener("click", () => {
+  modal?.classList.remove("active");
+  document.body.style.overflow = "";
+});
+
+modal?.addEventListener("click", (event) => {
+  if (event.target === modal) {
     modal?.classList.remove("active");
     document.body.style.overflow = "";
-  });
-
-  modal?.addEventListener("click", (event) => {
-    if (event.target === modal) {
-      modal?.classList.remove("active");
-      document.body.style.overflow = "";
-    }
-  });
+  }
 });
+// });
